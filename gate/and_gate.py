296c084d4d68b7e753d3297a4aa136ad5a305225
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-07-16 19:11:00 +0430

    make debug mode flag to false

--- gate/and_gate.py
+++ gate/and_gate.py
@@ -2,7 +2,7 @@ from gate.gate import Gate
 
 
 class And(Gate):
-    DEBUGMODE = True
+    DEBUGMODE = False
 
     def __init__(self, inputs: tuple, name="And_Gate"):
         super().__init__(inputs, name)
